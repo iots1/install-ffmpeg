496bd1a73a456d9557d305b328fa895ce41c51c7
iots1 <90421364+iots1@users.noreply.github.com>
2024-03-08 12:07:22 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+add-apt-repository ppa:ubuntuhandbook1/ffmpeg6
+apt update
+apt -y install ffmpeg
+ffmpeg -version
+
+cd /opt
+curl -sL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+chmod +x nodesource_setup.sh
+./nodesource_setup.sh 
+apt install nodejs
+npm install yarn -g
+apt update
+
+npm install pm2@latest -g
